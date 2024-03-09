f61d53ff36b0d68692012ab70beb0765dd1eec51
Hasan Yiğit Kaya <yigit.kaya@mobilistbilisim.com>
2024-03-09 16:23:51 +0300

    starting to write tests

--- lib/features/anime_list/bloc/cubit/filter_cubit.dart
+++
@@ -1,8 +0,0 @@
-import 'package:bloc/bloc.dart';
-import 'package:freezed_annotation/freezed_annotation.dart';
-
-part 'filter_state.dart';
-
-class FilterCubit extends Cubit<String> {
-  FilterCubit() : super("");
-}
--- lib/features/anime_list/bloc/cubit/filter_state.dart
+++
@@ -1,6 +0,0 @@
-part of 'filter_cubit.dart';
-
-// @freezed
-// class FilterState with _$FilterState {
-//   const factory FilterState.initial() = _Initial;
-// }
