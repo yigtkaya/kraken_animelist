6b0db23fe39a1f4bb822c555d899ff26f3bfb38b
Hasan Yiğit Kaya <yigit.kaya@mobilistbilisim.com>
2024-03-13 00:53:11 +0300

    Update dependencies and fix imports

--- test/blocs/kraken_anime_bloc/kraken_anime_bloc_test.dart
+++ test/blocs/kraken_anime_bloc/kraken_anime_bloc_test.dart
@@ -6,6 +6,7 @@ import 'package:kraken_animelist/features/anime_list/bloc/kraken_anime_event.dar
 import 'package:kraken_animelist/features/anime_list/bloc/kraken_anime_state.dart';
 import 'package:kraken_animelist/features/anime_list/data/repository/anime_list_repository_impl.dart';
 import 'package:kraken_animelist/features/anime_list/domain/models/kraken_anime_response.dart';
+import 'package:kraken_animelist/features/anime_list/domain/models/pagination.dart';
 
 class MockKrakenAnimeRepository extends Mock implements KrakenAnimeRepositoryImpl {}
 
@@ -31,6 +32,17 @@ void main() {
       act: (bloc) => bloc.add(const AppStartEvent()),
       expect: () => [isA<KrakenAnimeStateLoaded>()],
     );
+    blocTest<KrakenAnimeBloc, KrakenAnimeState>(
+      'emits [KrakenAnimeStateLoaded] when AppStartEvent is added but received error',
+      build: () => krakenAnimeBloc,
+      setUp: () => when(() => krakenAnimeRepository.getAnimeList(1)).thenThrow(Exception()),
+      act: (bloc) => bloc.add(
+        const AppStartEvent(),
+      ),
+      expect: () => [
+        isA<KrakenAnimeStateError>(),
+      ],
+    );
 
     blocTest<KrakenAnimeBloc, KrakenAnimeState>(
       'emits [KrakenAnimeStateLoaded] when PullToRefreshEvent is added',
@@ -41,5 +53,20 @@ void main() {
       act: (bloc) => bloc.add(const PullToRefreshEvent()),
       expect: () => [isA<KrakenAnimeStateLoaded>()],
     );
+
+    blocTest<KrakenAnimeBloc, KrakenAnimeState>(
+      'emits [KrakenAnimeStateLoaded] when PullToRefreshEvent is added but received a error',
+      build: () {
+        when(() => krakenAnimeRepository.getAnimeList(1)).thenThrow(Exception());
+        return KrakenAnimeBloc(krakenAnimeRepository);
+      },
+      act: (bloc) => bloc.add(const PullToRefreshEvent()),
+      expect: () => [isA<KrakenAnimeStateError>()],
+    );
+
+    test('Update page to zero', () {
+      krakenAnimeBloc.updatePage(2);
+      expect(krakenAnimeBloc.page, equals(2));
+    });
   });
 }
